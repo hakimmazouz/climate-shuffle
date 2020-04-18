d3158966886ed84ba1e98e48a4e6eae38cb3bbbe
AbdulHakim Mazouz <hakim@nomadic.dk>
2020-04-18 21:55:22 +0200

    new dpr

--- src/utils/const.js
+++ src/utils/const.js
@@ -13,4 +13,5 @@ export const SECTIONS = {
     },
 };
 
-export const DPR = window.innerWidth <= 2560 ? 2 : 1.5;
+export const DPR =
+    window.innerWidth <= 2560 ? Math.max(window.devicePixelRatio, 2) : 1.5;
